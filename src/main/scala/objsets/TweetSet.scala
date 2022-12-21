47d537528fa2b162b99c6d41870a9dbe6663a847
mkiroshdz <mkiroshdz@gmail.com>
2022-12-20 23:43:13 -0500

    Implement GoogleVsApple

--- src/main/scala/objsets/TweetSet.scala
+++ src/main/scala/objsets/TweetSet.scala
@@ -208,14 +208,14 @@ object GoogleVsApple:
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = allTweets.filter(twt => google.exists(w => twt.text.contains(w)))
+  lazy val appleTweets: TweetSet = allTweets.filter(twt => apple.exists(w => twt.text.contains(w)))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-  lazy val trending: TweetList = ???
+  lazy val trending: TweetList = (googleTweets union appleTweets).descendingByRetweet
 
 object Main extends App:
   // Print the trending tweets
